78103232503f195be53f10f886090182bb282c04
Güray Dağ <dagguray@gmail.com>
2024-07-31 11:54:50 +0300

    0.2.2
    footer and header fixed for mobile devices. general css adjustments

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -5,54 +5,50 @@ const Footer = () => {
   return (
     <>
       <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div className="col-md-4 d-flex align-items-center">
-            <Link
-              href="/"
-              className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
-            >
+        <footer className="d-flex py-3 border-top">
+          <div className="me-auto d-flex align-items-center">
+            <Link href="/" className="d-flex align-items-center">
               <img
-                className="bi"
+                className="bi me-2"
                 src="/assets/imgs/star.png"
                 style={{ opacity: "75%", width: "19px" }}
-              ></img>
+              />
             </Link>
-            <div className="mb-3 mb-md-0 text-white-50">
-              <span className="copyleft">&copy;</span>
+            <div className="text-white-50">
+              <span className="copyleft me-1">&copy;</span>
               2024 leidorf
             </div>
           </div>
 
-          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-            {/* instagram */}
-            <li className="ms-3">
-              <Link
-                className="text-body-secondary"
-                href="https://www.instagram.com/guraydg/"
-                target="_blank"
-              >
-                <img
-                  className="bi"
-                  src="/assets/imgs/instagram.png"
-                  style={{ opacity: "55%", width: "19px" }}
-                ></img>
-              </Link>
-            </li>
-            {/* github */}
-            <li className="ms-3">
-              <Link
-                className="text-body-secondary"
-                href="https://github.com/leidorf"
-                target="_blank"
-              >
-                <img
-                  className="bi"
-                  src="/assets/imgs/github.jpeg"
-                  style={{ opacity: "55%", width: "19px" }}
-                ></img>
-              </Link>
-            </li>
-          </ul>
+          {/* instagram */}
+          <div className="d-flex align-items-center">
+            <Link
+              className="text-body-secondary d-flex align-items-center"
+              href="https://www.instagram.com/guraydg/"
+              target="_blank"
+            >
+              <img
+                className="bi me-3"
+                src="/assets/imgs/instagram.png"
+                style={{ opacity: "55%", width: "19px" }}
+              />
+            </Link>
+          </div>
+
+          {/* github */}
+          <div className="d-flex align-items-center">
+            <Link
+              className="text-body-secondary d-flex align-items-center"
+              href="https://github.com/leidorf"
+              target="_blank"
+            >
+              <img
+                className="bi me-3"
+                src="/assets/imgs/github.jpeg"
+                style={{ opacity: "55%", width: "19px" }}
+              />
+            </Link>
+          </div>
         </footer>
       </div>
     </>
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,35 +1,39 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 
 const Header = () => {
   return (
     <>
       <div className="container">
-        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <Link
-            href="/"
-            className="d-flex align-items-center me-md-auto text-decoration-none link-light link-opacity-50-hover"
-          >
-            <img
-              className="bi me-2"
-              src="/assets/imgs/star.png"
-              style={{ width: "25px", opacity:"75%" }}
-            ></img>
-            <span className="fs-4">leidorf</span>
-          </Link>
-
-          <ul className="nav nav-pills">
-            <li className="nav-link">
-              <Link href="/works" className="nav-link link-light link-opacity-50-hover">
-                works
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link href="/about" className="nav-link link-light link-opacity-50-hover">
-                about
-              </Link>
-            </li>
-          </ul>
+        <header className="d-flex py-3 mb-3 border-bottom">
+          <div className="me-auto p-2">
+            <Link
+              href="/"
+              className="d-flex align-items-center text-decoration-none link-light link-opacity-50-hover"
+            >
+              <img
+                className="bi me-2"
+                src="/assets/imgs/star.png"
+                style={{ width: "25px", opacity: "75%" }}
+              ></img>
+              <span className="fs-4">leidorf</span>
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              href="/works"
+              className="nav-link link-light link-opacity-50-hover"
+            >
+              works
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              href="/about"
+              className="nav-link link-light link-opacity-50-hover"
+            >
+              about
+            </Link>
+          </div>
         </header>
       </div>
     </>
